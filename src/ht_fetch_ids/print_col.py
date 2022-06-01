4e5b06be9f9e7db70872214fef48b88cfb3a7836
Nick <versteeg@ohio.edu>
2022-06-01 18:51:49 -0400

    Add column naming options to print_col

--- src/ht_fetch_ids/print_col.py
+++ src/ht_fetch_ids/print_col.py
@@ -21,10 +21,26 @@ def main() -> int:
     parser.add_argument(
         "--value-sep", type=str, default="; ", help="The column value separator",
     )
+    parser.add_argument(
+        "--with-name",
+        action="store_true",
+        help="Print the column name at the top of the column",
+    )
+    parser.add_argument(
+        "--with-new-name",
+        type=str,
+        help="Print the supplied name at the top of the column",
+    )
     args = parser.parse_args()
 
     with open(args.csv_path, mode="r", encoding="utf-8") as fp:
         reader = csv.DictReader(fp, dialect=args.dialect)
+
+        if args.with_new_name:
+            print(args.with_new_name)
+        elif args.with_name:
+            print(args.column_name)
+
         for row in reader:
             values = row[args.column_name].split(args.value_sep)
             for value in values:
