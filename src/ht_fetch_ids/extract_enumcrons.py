68dc90c663fb97681e68ccdcf69209321a375796
Nick <versteeg@ohio.edu>
2022-06-02 16:18:07 -0400

    Allow extract-enumcrons to read from stdin

--- src/ht_fetch_ids/extract_enumcrons.py
+++ src/ht_fetch_ids/extract_enumcrons.py
@@ -2,6 +2,7 @@ import csv
 import sys
 import argparse
 import dataclasses
+import io
 
 from pathlib import Path
 
@@ -15,27 +16,36 @@ def main() -> int:
     parser.add_argument(
         "enumcrons_path",
         type=Path,
+        nargs="?",
         help="Path to the newline separated batch of enumcrons",
     )
     args = parser.parse_args()
 
-    with open(args.enumcrons_path, mode="r", encoding="utf-8") as fp:
-        writer = csv.DictWriter(
-            sys.stdout,
-            fieldnames=["enumcron"]
-            + [field.name for field in dataclasses.fields(ht_fetch_ids.Enumcron)],
-            dialect="excel-tab",
-        )
-        writer.writeheader()
-        for line in fp:
-            enumcron = line.strip()
-            writer.writerow(
-                {
-                    "enumcron": enumcron,
-                    **dataclasses.asdict(ht_fetch_ids.extract_enumcron(enumcron)),
-                }
-            )
+    if args.enumcrons_path:
+        with open(args.enumcrons_path, mode="r", encoding="utf-8") as fp:
+            write_extracted_enumcrons(fp)
+    else:
+        write_extracted_enumcrons(sys.stdin)
+
     return 0
+
+
+def write_extracted_enumcrons(fp: io.StringIO) -> None:
+    writer = csv.DictWriter(
+        sys.stdout,
+        fieldnames=["enumcron"]
+        + [field.name for field in dataclasses.fields(ht_fetch_ids.Enumcron)],
+        dialect="excel-tab",
+    )
+    writer.writeheader()
+    for line in fp:
+        enumcron = line.strip()
+        writer.writerow(
+            {
+                "enumcron": enumcron,
+                **dataclasses.asdict(ht_fetch_ids.extract_enumcron(enumcron)),
+            }
+        )
 
 
 if __name__ == "__main__":
